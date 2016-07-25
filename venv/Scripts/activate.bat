b514d58eca1a86b38da7086a380b48b05a4efac8
aixiamomo <920534583@qq.com>
2016-07-25 16:57:49 +0800

    7.25

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Riven\PycharmProjects\flasky\venv"
+set "VIRTUAL_ENV=C:\Users\vip\PycharmProjects\flasky\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
