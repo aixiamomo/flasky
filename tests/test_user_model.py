06650ad1d5153dffcbad0b04fd9039d65b442091
aixiamomo <920534583@qq.com>
2016-07-29 16:50:32 +0800

    默认的

---
+++ tests/test_user_model.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+import unittest
+from app.models import User
+
+
+class UserModelTestCase(unittest.TestCase):
+    def test_password_setter(self):
+        u = User(password='cat')
+        self.assertTrue(u.password_hash is not None)
