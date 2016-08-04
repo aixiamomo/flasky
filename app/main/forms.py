e38d4ed48a3b139cdb4e9496237719f1b68860fa
aixiamomo <920534583@qq.com>
2016-08-04 23:13:59 +0800

    10

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from flask_wtf import Form
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, Regexp
 
 
 class NameForm(Form):
@@ -12,6 +12,10 @@ class NameForm(Form):
 class EditProfileForm(Form):
     """用户级别的资料编辑表单"""
     name = StringField('Real name', validators=[Length(0, 64)])
-    location = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(Form):
+    """管理员级别的资料编辑表单"""
--- app/main/views.py
+++ app/main/views.py
@@ -48,10 +48,10 @@ def edit_profile():
     if form.validate_on_submit():
         current_user.name = form.name.data
         current_user.location = form.location.data
-        current_user.about_me = form.location.data
+        current_user.about_me = form.about_me.data
         db.session.add(current_user)
         flash('Your profile has been updated.')
-        return redirect(url_for('.user'), username=current_user.username)
+        return redirect(url_for('main.user', username=current_user.username))
     form.name.data = current_user.name
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
